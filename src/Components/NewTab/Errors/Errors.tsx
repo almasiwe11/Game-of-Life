0c00f55d271d02be47dd22c7cdf0d8f50448f50f
Almas Akhmetov <awakenedduck@gmail.com>
2023-09-17 12:20:05 +0600

    added errors form

--- src/Components/NewTab/Errors/Errors.tsx
+++ src/Components/NewTab/Errors/Errors.tsx
@@ -1,11 +1,35 @@
 import { FormikErrors } from "formik"
 import { TabTypesForm } from "../../../Types/TabTypes"
 
-export default function Errors({
-  errors,
-}: {
+type PropTypes = {
   errors: FormikErrors<TabTypesForm>
-}) {
-  console.log(errors)
-  return <div>Errors</div>
+}
+
+function getErrorMessages(object: FormikErrors<TabTypesForm>) {
+  const values: string[] = []
+
+  function getVal(object: FormikErrors<TabTypesForm>) {
+    for (const key in object) {
+      const oneKey = object[key as keyof FormikErrors<TabTypesForm>]
+      if (typeof oneKey === "object") {
+        getVal(oneKey)
+      } else {
+        values.push(oneKey!)
+      }
+    }
+  }
+
+  getVal(object)
+  return values
+}
+
+export default function Errors({ errors }: PropTypes) {
+  const errArr = getErrorMessages(errors)
+  return (
+    <ul className="mt-5 grid grid-cols-4 text-red-500 list-disc">
+      {errArr.map((err) => (
+        <li key={err}>{err}</li>
+      ))}
+    </ul>
+  )
 }
