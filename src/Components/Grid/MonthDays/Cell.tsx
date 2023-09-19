e18f511cb775cbc9f5942c18e6b0c51cbd95c83b
Almas Akhmetov <awakenedduck@gmail.com>
2023-09-19 13:07:40 +0600

    bug resolved

--- src/Components/Grid/MonthDays/Cell.tsx
+++ src/Components/Grid/MonthDays/Cell.tsx
@@ -39,7 +39,7 @@ export default function Cell({ date }: PropTypes) {
 
   useEffect(() => {
     setRating(getInitialRating({ thisTab, marked }))
-  }, [currentTab, setRating, getInitialRating])
+  }, [currentTab, setRating, marked, thisTab, today])
 
   useEffect(() => {
     if (addGoal) {
