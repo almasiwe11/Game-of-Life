89ed81f64a5b6e936a363529d679216978ea0e56
Almas Akhmetov <awakenedduck@gmail.com>
2023-10-09 10:59:37 +0600

    moved grid styles to a seperate ts file

--- src/Layout/Body/Grid/GridCell.tsx
+++ src/Layout/Body/Grid/GridCell.tsx
@@ -3,9 +3,8 @@ import useCell from "../../../Hooks/useCell"
 import { openMarkDay } from "../../../Calendar/calendarSlice"
 import { RootState } from "../../../RootState"
 import { isSameDay, isSameMonth, parseJSON } from "date-fns"
-import { moodIcons } from "../../../Icons/Icons"
-import { IconType } from "react-icons"
 import { Mood } from "../../../Types/HabitTypes"
+import { styleCell } from "./styleCell"
 
 type PropTypes = {
   date: Date
@@ -18,42 +17,19 @@ export default function Cell({ date }: PropTypes) {
   const { outOfScope, isStartDay, isToday, formattedDay } = useCell(date)
 
   function handleClick() {
-    if (!outOfScope) {
-      dispatch(openMarkDay(JSON.stringify(date)))
-    }
+    if (outOfScope) return
+    dispatch(openMarkDay(JSON.stringify(date)))
   }
 
   const isMarked = habit.markedDays
     .find((day) => isSameMonth(parseJSON(day.month), date))
     ?.marked.find((day) => isSameDay(parseJSON(day.date), date))
 
-  let Icon!: IconType
-  let background!: string
-  if (isMarked) {
-    if (isMarked.mood >= Mood.Extraordinary) {
-      Icon = moodIcons[moodIcons.length - 1].icon
-      background = moodIcons[moodIcons.length - 1].bg
-    } else if (
-      isMarked.mood >= Mood.Perfect &&
-      isMarked.mood < Mood.Extraordinary
-    ) {
-      Icon = moodIcons[Mood.Perfect].icon
-      background = moodIcons[Mood.Perfect].bg
-    } else {
-      Icon = moodIcons[isMarked.mood].icon
-      background = moodIcons[isMarked.mood].bg
-    }
-  }
-
-  const bg = {
-    backgroundColor: isMarked ? background : "rgb(30, 31, 33)",
-  }
+  const { bg, Icon } = styleCell(isMarked)
 
   return (
     <div
-      className={`text-center text-white border border-border relative cursor-pointer flex-center ${
-        isMarked ? `bg-${background}` : "bg-dark"
-      }`}
+      className={`text-center text-white border border-border relative cursor-pointer flex-center `}
       onClick={handleClick}
       style={bg}
     >
---
+++ src/Layout/Body/Grid/styleCell.ts
@@ -0,0 +1,32 @@
+import { IconType } from "react-icons"
+import { Mood } from "../../../Types/HabitTypes"
+import { moodIcons } from "../../../Icons/Icons"
+import { MarkedDaysOfMonth } from "../../../Types/CalendarType"
+
+function styleCell(isMarked: MarkedDaysOfMonth | undefined) {
+  let Icon!: IconType
+  let background!: string
+  if (isMarked) {
+    if (isMarked.mood >= Mood.Extraordinary) {
+      Icon = moodIcons[moodIcons.length - 1].icon
+      background = moodIcons[moodIcons.length - 1].bg
+    } else if (
+      isMarked.mood >= Mood.Perfect &&
+      isMarked.mood < Mood.Extraordinary
+    ) {
+      Icon = moodIcons[Mood.Perfect].icon
+      background = moodIcons[Mood.Perfect].bg
+    } else {
+      Icon = moodIcons[isMarked.mood].icon
+      background = moodIcons[isMarked.mood].bg
+    }
+  }
+
+  const bg = {
+    backgroundColor: isMarked ? background : "rgb(30, 31, 33)",
+  }
+
+  return { bg, Icon }
+}
+
+export { styleCell }
